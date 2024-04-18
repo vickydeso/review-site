e0a5c67849f8f3a188b0f14b440b88cd4f55b7af
vickydeso <113132217+vickydeso@users.noreply.github.com>
2024-04-18 13:42:26 -0400

    commits nuxt.js

---
+++ components/MovieList.vue
@@ -0,0 +1,27 @@
+<script setup>
+
+const BASE_URL = 'http://localhost:1337'
+
+defineProps({
+  movies: Object
+})
+</script>
+
+<template>
+  <div class="movie-list">
+    <ul>
+      <li :key="movie.id" v-for="movie in movies.data" class="movie-item">
+        <div class="movie-item-info">
+          <h2>{{ movie.attributes.title }}</h2>
+          <p>{{ movie.attributes.description }}</p>
+        </div>
+        <div class="movie-item-img">
+            <img :src="BASE_URL + movie.attributes.imgURL.data.attributes.formats.small.url" :alt="movie.attributes.title">
+        </div>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<style scoped>
+</style>
---
+++ nuxt-app/app.vue
@@ -0,0 +1,7 @@
+<template>
+  <MovieList :movies="data" />
+</template>
+
+<script setup>
+const { data } = await useFetch('http://localhost:1337/api/movies?populate=imgURL')
+</script>
