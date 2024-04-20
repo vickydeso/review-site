338c9eced9f6f589ad0165d4ec1ada25f93bd326
vickydeso <113132217+vickydeso@users.noreply.github.com>
2024-04-19 20:40:39 -0400

    updates components

--- nuxt-app/components/Search.vue
+++ nuxt-app/components/Search.vue
@@ -1,33 +1,32 @@
-<template>
-    <div class="container">
-      <header>
-        <input type="text" v-model="searchQuery" placeholder="Search movies...">
-      </header>
-  
-      <main>
-        <Suspense>
-          <MovieList v-if="filteredMovies.length > 0" :movies="filteredMovies" />
-          <h1 v-else>No movies found</h1>
-        </Suspense>
-      </main>
-    </div>
-  </template>
-  
-  <script setup>
-    import { ref, computed } from 'vue';
-  
-    const { data } = await useFetch('https://review-site-txzr.onrender.com/api/movies?populate=imgURL');
-  
-    const searchQuery = ref('');
-  
-    const filteredMovies = computed(() => {
-      return data ? data.filter(movie => {
-        return movie.attributes.title.toLowerCase().includes(searchQuery.value.toLowerCase());
-      }) : [];
-    });
-  </script>
-  
-  <style scoped>
-  </style>
+<script setup>
+  import { ref, computed } from 'vue';
+
+  let data = [];
+  let error = null;
+
+  const fetchData = async () => {
+    const response = await useFetch('https://review-site-txzr.onrender.com/api/movies?populate=imgURL');
+    if (response instanceof Error) {
+      error = response;
+      console.error('Error fetching data:', error);
+    } else {
+      data = response.data;
+    }
+  };
+
+  fetchData(); // Call the fetchData function to initiate data fetching
+
+  console.log('Data:', data); // Log data to inspect its structure
+
+  const searchQuery = ref('');
+
+  const filteredMovies = computed(() => {
+    return data ? data.filter(movie => {
+      return movie.attributes.title.toLowerCase().includes(searchQuery.value.toLowerCase());
+    }) : [];
+  });
+</script>
+
+
 
 Home.vue
